417727f964ebf8c339c5198af9c136b8ae0afee4
Sumit Patil <sumitpatil17020@gmail.com>
2026-07-22 21:06:36 +0530

    Add the some feature in Laboratory user panel

--- src/Pages/PatientRegistation/PatientRegistation.jsx
+++ src/Pages/PatientRegistation/PatientRegistation.jsx
@@ -1,6 +1,8 @@
 import "./PatientRegistation.css";
 import { labUserSidebarItem } from "../../Data/SidebarMenu";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import Card from "../../Components/Card/Card"
+import Inputfield from "../../Components/Inputfield/Inputfield";
 
 export default function PatientRegistation() {
   return (
@@ -11,7 +13,25 @@ export default function PatientRegistation() {
         <div className="Patient-content">
           <h1>Patient Registration</h1>
           <p className="subTitle-Patient">Welcome to Blood Laboratory Management System</p>
+        
+        <div className="card-outer-manage">
+            <Card>
+                <div className="card-inner-manage">
+                  <Inputfield label="Enter Patient Name" type="text" 
+                  placeholder="Enter Patient Name" className="patient-name"  />
+
+                  <Inputfield label="Enter Age" type="number" 
+                  placeholder="Enter Age" className="patient-age" />
+
+                  <Inputfield label="Enter Mobile Number" type="number"
+                  placeholder="Enter Mobile Number" />
+
+
+                </div>
+
+            </Card>
         </div>
+      </div>
       </div>
     </>
   );
